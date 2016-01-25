a6807df4543a5b94e6aa55118c2fe9f483176b46
MartinSom <martinsommer@seznam.cz>
2016-01-25 14:54:24 +0100

    Project

---
+++ ToDoList3/ToDoList3/www/scripts/app.js
@@ -0,0 +1,125 @@
+﻿(function () {
+    "use strict";
+
+    // To Do list module
+    angular.module('ToDoList', []);
+
+    // Main Controller
+    angular.module('ToDoList').controller('ToDoListController', function ($scope, FireBaseService) {
+
+        $scope.items = [];
+
+        $scope.selectedItems = [];
+
+        // edit-item-form uses the 'newItem' property to create new list item
+        $scope.newItem = {};
+
+        $scope.formVisibility = false;
+
+        // load list items form service
+        FireBaseService.loadToDoItems(function (toDoItems) {
+            $scope.items = toDoItems;
+            $scope.$apply();
+        });
+
+        $scope.selectItem = function (item1) {
+            $scope.hideForm();
+            if ($scope.itemIsSelected(item1) >= 1) {
+                $scope.selectedItems = $scope.selectedItems.filter(function (item) {
+                    return item.id != item1.id;
+                });
+            } else {
+                $scope.selectedItems.push(item1);
+            };
+        }
+
+        $scope.itemIsSelected = function (item1) {
+            return $scope.selectedItems.filter(function (item) {
+                return item.id == item1.id;
+            }).length;
+        };
+
+        $scope.isSelected = function () {
+            return $scope.selectedItems.length;
+        };
+
+        $scope.clearSelection = function () {
+            return $scope.selectedItems = [];
+        };
+
+        // delete selected item
+        $scope.closeSelectedItems = function (item) {
+
+            var lengthSelItems = $scope.selectedItems.length;
+            for (var i = 0; i < lengthSelItems; i++) {
+                FireBaseService.DeleteToDoItem($scope.selectedItems[i]);
+            }
+            
+            $scope.clearSelection();
+            $scope.items = FireBaseService.getToDoItems();
+        };
+
+        // create new item
+        $scope.createItem = function () {
+            FireBaseService.CreateToDoItem($scope.newItem);
+            $scope.items = FireBaseService.getToDoItems();
+            $scope.hideForm();
+            $scope.newItem = {};
+        };
+
+        // show form for creating item
+        $scope.showForm = function () {
+            $scope.formVisibility = true;
+            cordova.plugins.Keyboard.show();
+            window.setTimeout(function () {
+                document.getElementById("textItem").focus();
+            }, 0);
+        };
+
+        $scope.hideForm = function () {
+            $scope.formVisibility = false;
+            cordova.plugins.Keyboard.close();
+        };
+
+        $scope.formIsVisible = function () {
+            return $scope.formVisibility;
+        };
+    });
+
+    /*
+        Directives
+    */
+
+    // list items directive
+    angular.module('ToDoList').directive('listItems', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'list-items.html'
+        };
+    });
+
+    // upper menu directive
+    angular.module('ToDoList').directive('upperMenu', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'upper-menu.html'
+        };
+    });
+
+    // bottom menu directive
+    angular.module('ToDoList').directive('bottomMenu', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'bottom-menu.html'
+        };
+    });
+
+    // edit form directive
+    angular.module('ToDoList').directive('editItemForm', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'edit-item-form.html'
+        };
+    });
+
+})();
---
+++ ToDoList3/ToDoList3/www/scripts/services.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    "use strict";
+
+    // Firebase service
+    angular.module('ToDoList').service('FireBaseService', function () {
+
+        var fireBaseRoot = new Firebase("https://flickering-fire-8520.firebaseio.com/ToDoItems");
+
+        var toDoItems = [];
+
+        // load list items from service
+        this.loadToDoItems = function (listener) {
+
+            fireBaseRoot.once("value", function (messageSnapshot) {
+                messageSnapshot.forEach(function (snapShotItem) {
+                    toDoItems.push({id:snapShotItem.key(), text:snapShotItem.val().text});
+                });
+
+                listener(toDoItems);
+            });
+        };
+        
+        this.getToDoItems = function () {
+            return toDoItems;
+        };
+
+        this.CreateToDoItem = function (item) {
+
+            // push to service
+            var newPostRef = fireBaseRoot.push();
+            newPostRef.set(item);
+
+            // add to toDoItems array
+            item.id = newPostRef.key();
+            toDoItems.push(item);
+        };
+
+        this.DeleteToDoItem = function (item1) {
+
+            // delete from service
+            fireBaseRoot.child(item1.id).remove();
+
+            // delete from toDoItems array
+            toDoItems = toDoItems.filter(function (item) {
+                return item.id !== item1.id;
+            });
+        };
+    });
+})();
